4b74268ac84af8964aefb0b76ae70bff068f9d3a
hd <18511890109@163.com>
2019-11-21 17:21:05 +0800

    add application agent

--- pkg/application/main.go
+++ pkg/application/main.go
@@ -1,6 +1,26 @@
-package application
+package main
 
 import (
-
+	"log"
+	"os"
+	
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"github.com/rancher/types/config"
 )
 
+var (
+	kubeconfig string = "/root/.kube/config" 
+)
+func main() {
+	//restConfig, err := rest.InClusterConfig()
+	restConfig, err := clientcmd.BuildConfigFromFlags("", kubeConfig)
+	if err != nil {
+		log.Fatalf("Get restconfig failed: %s", err.Error())
+		os.Exit(1)
+	}
+	
+	userContext, err := config.NewUserOnlyContext(*restConfig)
+	
+	
+}
