8ac2d12186c6ef5f7e18a10310ff980a6b676772
hd <18511890109@163.com>
2019-11-24 22:19:41 +0800

    fix

--- pkg/application/controller/controller.go
+++ pkg/application/controller/controller.go
@@ -4,15 +4,15 @@ import (
 	"fmt"
 	
 	"github.com/rancher/types/config"
-	"github.com/rancher/types/apis/project.cattle.io/v3/"
 	"github.com/rancher/types/apis/core/v1"
 	"github.com/rancher/types/apis/apps/v1beta2"
 	"k8s.io/apimachinery/pkg/runtime"
+	"github.com/rancher/types/apis/project.cattle.io/v3/"
 )
 
 type controller struct {
-	applicationClient     v3.ApplicationInterface
-	applicationLister     v3.ApplicationLister
+	applicationClient     projectv3.ApplicationInterface
+	applicationLister     projectv3.ApplicationLister
 	namespaces            v1.NamespaceInterface
 	coreV1                v1.Interface
 	appsV1beta2           v1beta2.Interface
@@ -30,13 +30,13 @@ func Register(ctx context.Context, userContext *config.UserOnlyContext) {
 	c.applicationClient.AddHandler(ctx, "applictionCreateOrUpdate", c.sync)
 }
 
-func (c *controller)sync(key string, app *v3.Application) (runtime.Object, error) {
+func (c *controller)sync(key string, *v3.Application) (runtime.Object, error) {
 	if app == nil {
 		return nil, nil
 	}
 	
 	fmt.Println(key)
-	fmt.Println(*app)
+	//fmt.Println(*app)
 	
 	return nil, nil
 } 
