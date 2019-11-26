55855f115e32e005f2d66594fd81b998085e99f0
hd <18511890109@163.com>
2019-11-26 18:33:30 +0800

    update application controller

--- pkg/application/controller/controller.go
+++ pkg/application/controller/controller.go
@@ -3,12 +3,16 @@ package controller
 import (
 	"fmt"
 	"context"
+	"strings"
 	
 	"github.com/rancher/types/config"
 	"github.com/rancher/types/apis/core/v1"
 	"github.com/rancher/types/apis/apps/v1beta2"
 	"k8s.io/apimachinery/pkg/runtime"
 	"github.com/rancher/types/apis/project.cattle.io/v3"
+	appsv1beta2 "k8s.io/api/apps/v1beta2"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	corev1 "k8s.io/api/core/v1"
 )
 
 type controller struct {
@@ -36,8 +40,53 @@ func (c *controller)sync(key string, app *v3.Application) (runtime.Object, error
 		return nil, nil
 	}
 	
-	fmt.Println(key)
-	fmt.Println(*app)
+	splitted := strings.Split(key, "/")
+	namespace := splitted[0]
+	name := splitted[1]
+	image := app.Spec.Components[0].Containers[0].Image
+	ownerRef := metav1.OwnerReference{
+		Name:       app.Name,
+		APIVersion: app.APIVersion,
+		UID:        app.UID,
+		Kind:       app.Kind,
+	}
 	
+	deploy := &appsv1beta2.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			OwnerReferences: []metav1.OwnerReference{ownerRef},
+			Namespace:       namespace,
+			Name:            name,
+			Labels: map[string]string{
+				"application": "application-test",
+			},
+		},
+		Spec: appsv1beta2.DeploymentSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"application": "application-test",
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"application": "application-test",
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						corev1.Container{
+							Name: name,
+							Image: image,
+						},
+					},
+				},
+			},
+		},
+	}
+	
+	_, err := c.appsV1beta2.Deployments("").Create(deploy)
+	if err != nil {
+		fmt.Printf("create deploy error: %s", err.Error())
+	}
 	return nil, nil
 }
